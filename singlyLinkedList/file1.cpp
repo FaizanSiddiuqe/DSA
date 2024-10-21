2541375630b137216ed34e15788957fa486e35e0
FaizanSiddiuqe <154720299+FaizanSiddiuqe@users.noreply.github.com>
2024-10-21 20:53:08 +0500

    Insert new node at the end of the node

---
+++ singlyLinkedList/file1.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+// making node
+class Node
+{
+public:
+    int data;
+    Node *next;
+    // constructor
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+// inserting value at the end of the node
+void insertAtEnd(Node *&head, int data)
+{
+    Node *newNode = new Node(data);
+
+    if (head == NULL)
+    {
+        head = newNode;
+    }
+    else
+    {
+        Node *temp = head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+// print all the nodes
+void printAllNodes(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << "Data of given Nodes:: " << temp->data << endl;
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+
+    // head ptr which store the address of first node
+
+    Node *head = NULL; // assign to null its not pointing to anyone
+
+    // creating the nodes
+    insertAtEnd(head, 12);
+    insertAtEnd(head, 13);
+    insertAtEnd(head, 14);
+
+    // printing the nodes
+    printAllNodes(head);
+
+    return 0;
+}
