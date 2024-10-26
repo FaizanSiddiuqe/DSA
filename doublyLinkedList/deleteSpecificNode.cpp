84564c729406735ac48bc2adbbb031882c2891cd
FaizanSiddiuqe <154720299+FaizanSiddiuqe@users.noreply.github.com>
2024-10-26 15:32:42 +0500

    optimize delte node logic

--- doublyLinkedList/deleteSpecificNode.cpp
+++ doublyLinkedList/deleteSpecificNode.cpp
@@ -45,39 +45,52 @@ void insertAtEnd(Node *&head, int data)
 // delete the specific node
 void deleteSpecificNode(Node *&head, int val)
 {
-        if (head == nullptr)
+    // check if linked list is exist
+    if (head == nullptr)
+    {
+        cout << "Linked list is not present\n";
+        return;
+    }
+    Node *temp = head;
+    while (temp != nullptr) // Loop until temp reaches the end of the list
+    {
+        if (temp->data == val) // Node with the value to be deleted is found
         {
-            cout << "Linked list is not present\n";
-          return;
-        }
-
-       if (head->data == val)
-        {
-           Node *nodeToDelete = head->prev;
-            head = head->next;
-            if (head->prev != nullptr)
+            // Case 1: Deleting the head node
+            if (temp->prev == nullptr)
             {
-               head->prev = nullptr;
+                Node *nodeToDelete = temp;
+                head = temp->next; // Update the head to the next node
+                if (head != nullptr)
+                {
+                    head->prev = nullptr; // Set the new head's previous pointer to null
+                }
+                temp = head;         // Update temp to the next node
+                delete nodeToDelete; // Free the memory of the deleted node
             }
-            delete nodeToDelete;
+            // Case 2: Deleting the tail node
+            else if (temp->next == nullptr)
+            {
+                Node *nodeToDelete = temp;
+                temp->prev->next = nullptr; // Update the previous node's next pointer to null
+                delete nodeToDelete;        // Free the memory of the deleted node
+                temp = nullptr;             // End the loop since the tail was deleted
+            }
+            // Case 3: Deleting a middle node
+            else
+            {
+                Node *nodeToDelete = temp;
+                temp->prev->next = temp->next; // Update the previous node's next pointer
+                temp->next->prev = temp->prev; // Update the next node's previous pointer
+                temp = temp->next;             // Move temp to the next node before deleting
+                delete nodeToDelete;           // Free the memory of the deleted node
+            }
         }
         else
         {
-            Node *temp = head;
-            while (temp->next != nullptr)
-            {
-                if (temp->data == val)
-                {
-                    temp->prev->next = temp->next;
-                    temp->next->prev = temp->prev;
-                    delete temp;
-                }
-                else
-                {
-                    temp = temp->next;
-                }
-            }
+            temp = temp->next; // Move to the next node if no match is found
         }
+    }
 }
 
 // print all elements of the node's
@@ -112,12 +125,11 @@ int main()
     insertAtEnd(head, 13);
     insertAtEnd(head, 14);
     insertAtEnd(head, 15);
+    insertAtEnd(head, 14);
     insertAtEnd(head, 12);
 
     // call the delete function
-    deleteSpecificNode(head, 14);
     deleteSpecificNode(head, 12);
-
     printElementsOfNodes(head);
     return 0;
 }
